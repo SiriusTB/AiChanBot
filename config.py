a1701d34f45f0eb566f5253f225a14768dcbe5a6
SiriusTB <116369337+SiriusTB@users.noreply.github.com>
2022-11-18 20:42:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
-api_hash = ""
+api_hash = "e54a67706f5a244d05a0cdf53e8c0efe"
 
-api_id = int("")
+api_id = int("10061893")
 
-bot_token = ""
+bot_token = "5759741996:AAEEnHzsUSZFwN27cwJhP3LJduRKQDI23fY"
 
-luna_key = ""
+luna_key = "Lunab9536450-c670-4045-a247-2db90f757e08"
 
 # Check readme for where to get key 
